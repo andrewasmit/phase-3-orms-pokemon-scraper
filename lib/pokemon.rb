eef8addd0412484b2c850e07b9c8633708294685
andrew <andrewasmit@hotmail.com>
2023-02-02 12:26:26 -0600

    commit tree 0cbfcf38aec31f3f0eaed20dfadcdd4de3ef7e35

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
+# require 'schema_migration.sql'
+
 class Pokemon
+attr_accessor :id, :name, :db, :type
+
+    def initialize(id: nil, name:, type:, db:)
+        @id=id
+        @name=name
+        @type=type
+        @db=db
+    end
+
 end
+ 
